509eb8907939d613e411cc73e043d476ebaa3c84
LiusDev <quydx.work@gmail.com>
2025-09-15 14:19:30 +0700

    fix: common validation schema

--- src/utils/commonValidationSchema.ts
+++ src/utils/commonValidationSchema.ts
@@ -6,8 +6,8 @@ export const searchAndPaginationSchema = z.object({
 		.enum(["id", "name", "description", "price", "product_group_id"])
 		.optional(),
 	order: z.enum(["asc", "desc"]).optional(),
-	page: z.number().min(1).default(1),
-	limit: z.number().min(1).max(100).default(10),
+	page: z.coerce.number().min(1).default(1),
+	limit: z.coerce.number().min(1).max(100).default(10),
 })
 // generate type from schema
 export type SearchAndPaginationSchema = z.infer<
