3e0f7f2624656e55732b00590f06b9a15b74f95e
LiusDev <quydx.work@gmail.com>
2025-09-15 16:34:36 +0700

    fix: del api

--- src/routes/products/index.ts
+++ src/routes/products/index.ts
@@ -111,7 +111,7 @@ products.delete(
 		try {
 			const { groupId } = c.req.valid("param")
 			await deleteProductGroup(c, { groupId })
-			return c.body(null, status.NO_CONTENT)
+			return c.status(status.NO_CONTENT)
 		} catch (err: any) {
 			console.error("Error in DELETE /products/:id:", err.message)
 			return error(c, {
@@ -369,7 +369,7 @@ products.delete(
 				})
 			}
 
-			return c.body(null, status.NO_CONTENT)
+			return c.status(status.NO_CONTENT)
 		} catch (err: any) {
 			console.error(
 				"Error in DELETE /products/:groupId/:productId:",
