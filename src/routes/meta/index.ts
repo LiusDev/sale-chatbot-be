43dfc7e5c68db1dc189bf3a9183b19deb000ba11
LiusDev <quydx.work@gmail.com>
2025-09-21 16:58:13 +0700

    fix: meta verify method

--- src/routes/meta/index.ts
+++ src/routes/meta/index.ts
@@ -4,6 +4,15 @@ import { AppContext } from "../../types/env"
 const meta = new Hono<AppContext>()
 
 meta.post("/webhook", async (c) => {
+	//  verify header:
+	const signature = c.req.header("X-Hub-Signature-256")
+	if (!signature) {
+		return c.json({ message: "Signature not found" }, 401)
+	}
+	console.log(signature)
+	if (signature !== c.env.META_VERIFY_SIGNATURE) {
+		return c.json({ message: "Invalid signature" }, 401)
+	}
 	const body = await c.req.json()
 	console.log(body)
 	return c.json({ message: "Webhook received" })
